5936710a1c86f16dede943af9a45b24ea2241ec5
Sahithi_Kakkerla@epam.com <StayDetermined@143>
2021-05-09 01:35:06 +0530

    added test cases

---
+++ artifactid/src/test/java/MyTest.java
@@ -0,0 +1,19 @@
+import junit.framework.Assert;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+
+public class MyTest {
+    @Test
+    public void test1(){
+        Assert.assertTrue(true);
+    }
+    @Test
+    public void test2(){
+        Assert.assertTrue(true);
+    }
+
+
+}
+
